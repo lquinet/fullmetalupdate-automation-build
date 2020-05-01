bc870a2e2ecdc600a94191158d18a4be2c2a8f61
lquinet <loicquinet@outlook.com>
2020-05-01 18:29:56 +0200

    Check if $BITBUCKET_USERNAME or $BITBUCKET_PASSWORD exist

--- docker/worker/docker-entrypoint.sh
+++ docker/worker/docker-entrypoint.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+if [ -z "$BITBUCKET_USERNAME" ] || [ -z "$BITBUCKET_PASSWORD" ]; then
+        echo '$BITBUCKET_USERNAME or $BITBUCKET_PASSWORD does not exist'
+		exit 1
+fi
+
 git clone https://$BITBUCKET_USERNAME:$BITBUCKET_PASSWORD@bitbucket.org/adeneo-embedded/farm-core.git
 
 cd farm-core/ && ./install.sh
