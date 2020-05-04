26a1b7ea8aae50e5c151e1f862381450932e1c9a
lquinet <loicquinet@outlook.com>
2020-05-04 15:19:22 +0200

    Remove previous farm-core installation

--- docker/worker/docker-entrypoint.sh
+++ docker/worker/docker-entrypoint.sh
@@ -7,10 +7,12 @@ if [ -z "$BITBUCKET_USERNAME" ] || [ -z "$BITBUCKET_PASSWORD" ]; then
 		exit 1
 fi
 
+# Install farm-core
+rm -rf farm-core
 git clone -b WUKF-223-support-new-version-pyftdi https://$BITBUCKET_USERNAME:$BITBUCKET_PASSWORD@bitbucket.org/adeneo-embedded/farm-core.git
-
 cd farm-core/ && ./install.sh
 
 cd /buildbot
 
+# Launch buildbot worker
 /usr/bin/dumb-init twistd --pidfile= -ny buildbot.tac
